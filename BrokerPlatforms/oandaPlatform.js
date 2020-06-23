9767be2f6edeea4511320ed60c1dc9005568874e
John Kyony <jkyony@gmail.com>
2020-06-23 16:06:50 +0200

    Initial Commit - stable

---
+++ BrokerPlatforms/oandaPlatform.js
@@ -0,0 +1,440 @@
+import axios from 'axios'
+
+
+
+const demoTradeAccountApiUrl = 'https://api-fxpractice.oanda.com/v3/'
+const realTradeAccountApiUrl = 'https://api-fxtrade.oanda.com/v3/'
+const apiUrl = demoTradeAccountApiUrl
+const streamApiUrl = "https://stream-fxtrade.oanda.com/v3/accounts/"
+const token = '54e39d6da0d6e2511b6a0930b268c906-4e50438d474c88ab5b223d497a91aca2'
+const demoToken = '559cf3288a5edc76c76ee080d130b810-c23454cbc30b4a395a0801a7b00ab581'
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + demoToken;
+// const config = {
+//     headers: {
+//         Authorization: 'Bearer ' + token
+//     }
+// }
+const Accounts = {
+    allAccounts: async function (){
+        //Get a list of all Accounts authorized for the provided token
+       
+        try {
+            let allAccountDetails = await axios.get(apiUrl + 'accounts' )
+            
+            
+            return allAccountDetails.data.accounts
+        }catch(err) {
+            console.log(err.error)
+        }
+        
+       
+        
+        // console.log(allAccountDetails.data)
+    } , 
+    accountDetails : async function (accountID) {
+        // Get the full details for a single account that a client has access to
+        // Full pending order , open Trade and open position representation provided
+        try {
+            let accountDetails = await axios.get(apiUrl+ `accounts/${accountID}`)
+            
+            return accountDetails.data
+        }catch(err) {
+            console.log(err.error)
+        }
+        
+        
+    } , 
+    accountDetailsSummary : async function (accountID) {
+        //Get a summarry for a single account that a client has access to
+        try {
+            
+            let accountDetailsSummary = await axios.get(apiUrl+ `accounts/${accountID}/summary`)
+            return accountDetailsSummary.data
+
+        } catch (err) {
+            console.log(err.error)
+        }
+        
+    } , 
+    accountInstrumentsDetails: async function (accountID) {
+        // Get the list of tradeable instruments for the given account 
+        // The list of tradeable instruments is dependent on the regulatory division that the account is located in 
+        // Same for all single user accounts
+        try{
+            let accountsInstrumentsDetails = await axios.get(apiUrl + `accounts/${accountID}/instruments`)
+            
+            return  accountsInstrumentsDetails.data
+        }catch (err){
+            console.log(err.error)
+        }
+        
+    }, 
+    accountLatestChanges: async function (accountID , sinceTransactionID) {
+        // Used to poll an Account for its current state & changes since a specificied TransactionId
+        try {
+            let accountLatestChanges = await axios.get(apiUrl + `accounts/${accountID}/changes?sinceTransactionID=${sinceTransactionID}`)
+            console.log(accountLatestChanges)
+            return accountLatestChanges.data
+
+        }catch(err) {
+            console.log(err.error)
+        }
+        
+    }
+}
+
+const Instruments = {
+    instrumentCandles: async function (instrument , startFrom){
+        // Fetch candle stick data for an instrument
+        try{
+            let instrumentCandles = await axios.get(apiUrl + `instruments/${instrument}/candles?count=10&price=A&from=2016-01-01T00%3A00%3A00.000000000Z&granularity=D`)
+            console.log(instrumentCandles.data)
+            return instrumentCandles.data
+
+        }catch(err){
+            console.log(err.error)
+        }
+        
+    }, 
+    instrumentOrderBook: async function (instrument) {
+        // Fetch an order book for an instrument 
+        try{
+            let instrumentOrderBook = await axios.get(apiUrl + `instruments/${instrument}/orderBook`)
+            
+            return instrumentOrderBook.data
+        }catch(err){
+            console.log(err.error)
+        }
+        
+    }, 
+    instrumentPositionBook: async function (instrument) {
+        // Fetch a position book for an instrument 
+        try{
+            let instrumentPositionBook = await axios.get(apiUrl + `instruments/${instrument}/positionBook`)
+            return instrumentPositionBook.data
+        }catch(err){
+            console.log(err.error)
+        }
+        
+    }
+}
+
+const Orders = {
+    createNewOrder: async function (accountID , units , instrument , timeInforce , type , positionFill , price) {
+        // Create an Order for an Account 
+        try {
+            
+            console.log('creating new order')
+            console.log('==================')
+            console.log(`Account Id: ${accountID} \n units: ${units} \n instrument: ${instrument} \n timeInforce: ${timeInforce} \n type: ${type} \n positionFill : ${positionFill} \n price: ${price}`)
+            let newOrder = await axios.post(apiUrl + `accounts/${accountID}/orders` , {
+                "order" : {
+                    "units" : units , 
+                    "instrument": instrument , 
+                    "timeInForce": timeInforce, 
+                    "type": type , 
+                    "positionFill": positionFill,
+                    "price": price
+                }
+               
+            })
+            console.log('===================')
+            console.log(newOrder.data)
+            console.log('===================')
+            return newOrder.data
+            
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+       
+    } , 
+    allAccountOrders: async function(accountID){
+        // Get a list of Orders for an Account 
+        try {
+            let allOrders = await axios.get(apiUrl + `accounts/${accountID}/orders`)
+            return allOrders.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+        
+
+    } , 
+    allPendingOrders: async function(accountID){
+        // List all pending Orders in an Account
+        try {
+            let allPendingOrders = await axios.get(apiUrl + `accounts/${accountID}/pendingOrders`)
+            return allPendingOrders.data
+
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    orderDetail: async function(accountID , orderSpecifier) {
+        // Get details for a single Order in an Account 
+        try {
+            let orderDetail = await axios.get(apiUrl + `accounts/${accountID}/orders/${orderSpecifier}`)
+            return orderDetail.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    orderCancel: async function (accountID , orderSpecifier){
+        // Cancel a pending Order in an Account 
+        try {
+            let orderCancel = await axios.put(apiUrl + `accounts/${accountID}/orders/${orderSpecifier}/cancel`)
+            return orderCancel.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    }
+}
+
+const Trades = {
+    allTrades : async function (accountID){
+        // Get a list of Trades for an Account 
+        try {
+            let allTrades = await axios.get(apiUrl + `accounts/${accountID}/trades`)
+            return allTrades.data
+
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    allOpenTrades : async function (accountID){
+        // Get a list of open trades for an account 
+        try {
+            let allOpenTrades = await axios.get(apiUrl + `accounts/${accountID}/openTrades`)
+            return allOpenTrades.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    tradeDetails : async function (accountID , tradeSpecifier){
+        // Get the details of a specific trade in an Account
+        try {
+            let tradeDetails = await axios.get(apiUrl + `accounts/${accountID}/trades/${tradeSpecifier}`)
+            return tradeDetails.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    closeTrade: async function (accountID ,  tradeSpecifier) {
+        // Close (partially or fully) a specific open trade in an Account
+        try {
+            console.log("==========")
+            console.log("Closing Trade with id: \t" + tradeSpecifier)
+            let closeTrade = await axios.put(apiUrl + `accounts/${accountID}/trades/${tradeSpecifier}/close`)
+            return closeTrade.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    trailingStopOnTrades: async function (accountID , tradeSpecifier , takeProfitPrice , stopLossPrice , timeInForce) {
+        // Create , replace and cancel a Trade's dependent Orders
+        //  ( Take Profit , Stop Loss and Trailing Stop Loss) 
+        // Through a trade itself 
+        try {
+            let trailingStopOnTrades = await axios.put(apiUrl + `accounts/${accountID}/trades/${tradeSpecifier}/orders` , {
+                "takeProfit" : {
+                    "timeInForce": timeInForce , 
+                    "price": takeProfitPrice
+                }, 
+                "stopLoss": {
+                    "timeInForce": timeInForce, 
+                    "price": stopLossPrice
+                }
+            })
+            return trailingStopOnTrades.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+        
+    }
+}
+
+const Positions = {
+    allPositions: async function (accountID){
+        // List all positions for an account . 
+        //The positions returned are for every instrument 
+        // that has had a position during the lifetime of an account 
+        try {
+            let allPositions = await axios.get(apiUrl + `accounts/${accountID}/positions`)
+            return allPositions.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    allOpenPositions: async function (accountID){
+        //List all open Positions for an Account . an open Position
+        // is a Position in an Account that currently has a trade opened for it.
+        try {
+            let allOpenPositions = await axios.get(apiUrl + `accounts/${accountID}/openPositions`)
+            return allOpenPositions.data
+        } catch (error) {
+            console.log(error.error)
+        }
+       
+    } , 
+    singlePositionDetails: async function (accountID , instrument) {
+        // Get the details of a single Instrument's Position 
+        // in an Account . The position may by open or not. 
+        try {
+            let singlePositionDetails = await axios.get(apiUrl + `accounts/${accountID}/positions/${instrument}`)
+            return singlePositionDetails.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    closeSinglePosition: async function (accountID , instrument , longUnits){
+        // close out the open Position for a specific instrument in an Account 
+        try {
+            let closeSinglePosition = await axios.put(apiUrl + `accounts/${accountID}/positions/${instrument}/close`, {
+                "longUnits": longUnits
+            })
+            return closeSinglePosition.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+        
+    }
+}
+
+const Transactions = {
+    allTransactions: async function (accountID) {
+        // Get a list of Transactions pages that satisfy a time-based Transation query
+        try {
+            let allTransactions = await axios.get(apiUrl + `accounts/${accountID}/transactions`)
+            return allTransactions
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    singleAccountTransactionDetails: async function (accountID , transactionID){
+        // Get the details of a single Acccount Transaction 
+        try {
+            let singleAccountTransactionDetails = await axios.get(apiUrl + `accounts/${accountID}/transactions/${transactionID}`)
+            return singleAccountTransactionDetails.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    searchTransactionsByIDRange: async function (accountID , idRangeFrom , idRangeTo){
+        // Get a rand of Transactions for an Account based on the transaction ids
+        try {
+            let searchTransactionsByIDRange = await axios.get(apiUrl + `accounts/${accountID}/transactions/idrange?to=${idRangeTo}&from=${idRangeFrom}`)
+            return searchTransactionsByIDRange.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    searchTransactionStartingFromSpecificID: async function (accountID , transactionID) {
+        // Get a range of Transactions for an Account starting at (but not including) a provided transaction Id
+        try {
+            let searchTransactionStartingFromSpecificID = await axios.get(apiUrl + `accounts/${accountID}/transactions/${transactionID}`)
+            return searchTransactionStartingFromSpecificID.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    streamLatestTransactions: async function (accountID) {
+        // Get a Stream of Transactions for an Account starting from when 
+        //the request is made NB Streaming End point 
+        try {
+            let streamLatestTransactions = await axios({
+                method: 'get' , 
+                url : streamApiUrl + `${accountID}/transactions/stream`,
+                responseType: 'stream',
+                 config
+    
+            })
+            return streamLatestTransactions.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+        
+
+    }
+}
+
+const Pricings = { 
+    latestCandlesResult: async function (accountID) {
+        //Get dancing bears and most recently completed candles within and 
+        // Account for speciied combinations of instruement , 
+        //granualarity , and price component 
+        try {
+            let latestCandlesResult = await axios.get(apiUrl + `accounts/${accountID}/candles/latest`)
+            return latestCandlesResult.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    }, 
+    specificPricingDetails: async function (accountID) {
+        // Get pricing information for a specified list of instruments within an account 
+        try {
+            let specificPricingDetails = await axios.get(apiUrl + `accounts/${accountID}/pricing`)
+            return specificPricingDetails.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    } , 
+    pricingStream: async function (accountID) {
+        // Get a stream of Account Prices starting from when the request is made. 
+        // This pricing stream does not include every single price created for the account ,
+        // Instead will provide at most 4 prices per second (every 250 milliseconds) for each instruments requested
+        // if > 1 price is created for an instrument during 250 millisecond window , only price in effect at the end of the window is sent
+        // This means that during periods of rapid movement , you will not see every price , depending on when you connected you shall see different prices
+        try {
+            let pricingStream = await axios({
+                method: 'get' ,
+                url : streamApiUrl + `${accountID}/pricing/stream`,
+                responseType: 'stream', 
+                config
+            })
+            return pricingStream.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+        
+    } , 
+    fetchInstrumentCandleResults: async function (accountID , instrument) {
+        // Fetch Candlestick data for an instrument 
+        try {
+            let fetchInstrumentCandleResults = await axios.get(apiUrl + `accounts/${accountID}/instruments/${instrument}/candles`)
+            return fetchInstrumentCandleResults.data
+        } catch (error) {
+            console.log(error.error)
+        }
+        
+    }
+
+}
+
+export default  {
+    Accounts,
+    Instruments, 
+    Orders,
+    Trades,
+    Positions,
+    Pricings
+}
---
+++ index.js
@@ -0,0 +1,389 @@
+import express from 'express'
+import cors from 'cors'
+import oanda from './BrokerPlatforms/oandaPlatform.js'
+import bodyParser from 'body-parser'
+
+const app = express ()
+
+app.use(cors())
+// parse application/json 
+app.use(bodyParser.json())
+// Accounts End Points
+app.get('/accounts/allAccounts' , async (req , res) => {
+    try {
+        let allAccounts = await oanda.Accounts.allAccounts()
+        res.send(allAccounts)
+        
+    }catch(err) {
+        console.log(err.error)
+    }
+
+})
+
+app.get('/accounts/:accountID/accountDetails' , async (req, res) => {
+    try {
+        let accountDetails = await oanda.Accounts.accountDetails(req.params.accountID)
+        res.send(accountDetails)
+
+    } catch (err) {
+        console.log(err.error)
+    }
+   
+  
+})
+
+
+app.get('/accounts/:accountID/accountDetailsSummary' , async (req , res) => {
+    try {
+        
+       let accountDetailsSummary = await oanda.Accounts.accountDetailsSummary(req.params.accountID)
+       res.send(accountDetailsSummary)
+    }catch(err) {
+        console.log(err.error)
+    }
+    
+})
+
+app.get('/accounts/:accountID/accountInstrumentsDetails' , async (req , res) => {
+    try {
+        let accountInstrumentsDetails = await oanda.Accounts.accountInstrumentsDetails(req.params.accountID)
+        res.send(accountInstrumentsDetails)
+    }catch(err) {
+        console.log(err.error)
+    }
+    
+})
+
+app.get('/accounts/:accountID/accountLatestChanges/:sinceTransactionID' , async (req , res) => {
+    try {
+        let accountLatestChanges = await oanda.Accounts.accountLatestChanges(req.params.accountID , req.params.sinceTransactionID)
+        res.send(accountLatestChanges)
+    }catch(err) {
+        console.log(err.error)
+    }
+   
+})
+
+
+// Instruments End Points 
+// app.get('/instruments/:instrument/instrumentCandles/:startFrom', async (req , res) => {
+//     try{
+        
+//         let instrumentCandles = await oanda.Instruments.instrumentCandles(req.params.instrument , req.params.startFrom)
+//         res.send(instrumentCandles)
+//     }catch(err) {
+//         console.log(err.error)
+//     }
+   
+
+// })
+
+app.get('/instruments/:instrument/instrumentOrderBook' ,async (req , res) => {
+    try {
+
+        let instrumentOrderBook = await oanda.Instruments.instrumentOrderBook(req.params.instrument)
+ 
+        res.send(instrumentOrderBook)
+    } catch(err){
+        console.log(err.error);
+        
+    }
+   
+})
+
+app.get('/instruments/:instrument/instrumentPositionBook' , async (req , res) => {
+    try {
+        let instrumentPositionBook = await oanda.Instruments.instrumentPositionBook(req.params.instrument)
+        res.send(instrumentPositionBook)
+    } catch (error) {
+        console.log(error.error)
+    }
+
+})
+
+// Orders 
+app.post('/orders/:accountID/createNewOrder' , async (req , res) => {
+    try {
+        let units = req.body.units 
+        let instrument = req.body.instrument
+        let timeInForce = req.body.timeInForce
+        let type = req.body.type 
+        let positionFill = req.body.positionFill
+        let accountID = req.params.accountID
+        let price = req.body.price
+        
+        let createNewOrder = await oanda.Orders.createNewOrder(accountID , units , instrument , timeInForce , type , positionFill , price)
+
+        res.send(createNewOrder)
+
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+    
+})
+
+app.get('/orders/:accountID/allAccountOrders' , async (req , res) => {
+    try {
+        let allAccountOrders = await oanda.Orders.allAccountOrders(req.params.accountID)
+        res.send(allAccountOrders)
+    } catch (error) {
+        console.log(error.error)
+        
+    }
+    
+})
+
+app.get('/orders/:accountID/allPendingOrders' , async (req , res) => {
+    try {
+        let allPendingOrders = await oanda.Orders.allPendingOrders(req.params.accountID)
+        res.send(allPendingOrders)
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/orders/:accountID/orderDetail/:orderSpecifier' , async (req , res) => {
+
+    try {
+        let orderDetail = await oanda.Orders.orderDetail(req.params.accountID , req.params.orderSpecifier)
+        res.send(orderDetail)
+    } catch (error) {
+        console.log(error.error)
+    }
+
+})
+
+app.get('/orders/:accountID/orderCancel/:orderSpecifier' , async (req , res) => {
+    try {
+        let orderCancel = await oanda.Orders.orderCancel(req.params.accountID , req.params.orderSpecifier)
+        res.send(orderCancel)
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+
+// Trades End Point 
+
+app.get('/trades/:accountID/allTrades' , async (req , res) => {
+    try {
+        let allTrades = oanda.Trades.allTrades(req.params.accountID)
+        res.send(allTrades)
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/trades/:accountID/allOpenTrades' , async (req , res) => {
+    try {
+        let allOpenTrades = await oanda.Trades.allOpenTrades(req.params.accountID)
+        res.send(allOpenTrades)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+   
+})
+
+app.get('/trades/:accountID/tradeDetails/:tradeSpecifier' , async (req , res) => {
+    try {
+        let tradeDetails = await oanda.Trades.tradeDetails(req.params.accountID , req.params.tradeSpecifier)
+        res.send(tradeDetails)
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.post('/trades/:accountID/closeTrade/:tradeSpecifier' , async (req , res) => {
+    try {
+        let closeTrade = await oanda.Trades.closeTrade(req.params.accountID , req.params.tradeSpecifier)
+        res.send(closeTrade)
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+    
+
+})
+
+app.post('/trades/:accountID/trailingStopOnTrades/:tradeSpecifier' , async (req , res) => {
+    try {
+        let takeProfitPrice = req.body.takeProfitPrice
+        let stopLossPrice = req.body.stopLossPrice
+        let timeInForce = req.body.timeInForce
+
+        let trailingStopOnTrades = await oanda.Trades.trailingStopOnTrades(req.params.accountID , req.params.tradeSpecifier , takeProfitPrice , stopLossPrice , timeInForce)
+        res.send(trailingStopOnTrades)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+
+    
+})
+
+// Positions End Points 
+app.get('/positions/:accountID/allPositions' , async (req , res) => {
+    try {
+        let allPositions = await oanda.Positions.allPositions(req.params.accountID)
+        res.send(allPositions)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/positions/:accountID/allOpenPositions' ,async (req , res) => {
+    try {
+        let allOpenPositions = await oanda.Positions.allOpenPositions(req.params.accountID)
+        res.send(allOpenPositions)
+             
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/positions/:accountID/singlePositionDetails/:instrument' ,async (req , res) => {
+    try {
+        let singlePositionDetails = await oanda.Positions.singlePositionDetails(req.params.accountID , req.params.instrument)
+        res.send(singlePositionDetails)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.post('/positions/:accountID/closeSinglePosition/:instrument', async (req , res) => {
+    try {
+        let longUnits = req.body.longUnits
+        let closeSinglePosition = await oanda.Positions.closeSinglePosition(req.params.accountID , req.params.instrument , longUnits)
+        res.send(closeSinglePosition)
+             
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+
+})
+
+
+// Transactions End Points 
+app.get('/transactions/:accountID/allTransactions' , async (req , res) => {
+    try {
+        let allTransactions = await oanda.Transactions.allTransactions(req.params.accountID)
+        res.send(allTransactions)
+             
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/transactions/:accountID/singleAccountTransactionDetails/:transactionID' , async (req , res) => {
+    try {
+        let singleAccountTransactionDetails = await oanda.Transactions.singleAccountTransactionDetails(req.params.accountID , req.params.transactionID)
+        res.send(singleAccountTransactionDetails)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.post('/transactions/:accountID/searchTransactionsByIDRange', async (req ,res) => {
+    try {
+        let idRangeFrom = req.body.idRangeFrom
+        let idRangeTo = req.body.idRangeTo
+
+        let searchTransactionsByIDRange = await oanda.Transactions.searchTransactionsByIDRange(req.params.accountID , idRangeFrom , idRangeTo)
+        res.send(searchTransactionsByIDRange)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+
+
+   
+})
+
+app.get('/transactions/:accountID/searchTransactionStartingFromSpecificID/:transactionID' ,  async (req , res) => {
+    try {
+        let searchTransactionStartingFromSpecificID = await oanda.Transactions.searchTransactionStartingFromSpecificID(req.params.accountID , req.params.transactionID)
+        res.send(searchTransactionStartingFromSpecificID)
+             
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/transactions/:accountID/streamLatestTransactions' ,async (req , res) => {
+    try {
+        let streamLatestTransactions = await oanda.Transactions.streamLatestTransactions(req.params.accountID)
+        res.send(streamLatestTransactions)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+// Pricings End Points 
+app.get('/pricings/:accountID/latestCandlesResult' , async (req , res) => {
+    try {
+        let latestCandlesResult = await oanda.Pricings.latestCandlesResult(accountID)
+        res.send(latestCandlesResult)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+   
+})
+
+app.get('/pricings/:accountID/specificPricingDetails' , async (req , res) => {
+    try {
+        let specificPricingDetails = await oanda.Pricings.specificPricingDetails(req.params.accountID)
+        res.send(specificPricingDetails)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/pricings/:accountID/pricingStream' , async (req , res) => {
+    try {
+        let pricingStream = await oanda.Pricings.pricingStream(req.params.accountID)
+        res.send(pricingStream)    
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+app.get('/pricings/:accountID/fetchInstrumentCandleResults/:instrument' , async (req, res) => {
+    try {
+        let fetchInstrumentCandleResults = await oanda.Pricings.fetchInstrumentCandleResults(req.params.accountID , req.params.instrument)
+        res.send(fetchInstrumentCandleResults)
+            
+    } catch (error) {
+        console.log(error.error)
+    }
+    
+})
+
+
+
+
+app.listen(3000 , () => 
+console.log('Example app listening on port 3000!')
+)
